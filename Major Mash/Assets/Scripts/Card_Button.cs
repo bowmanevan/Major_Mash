2bbb017e31848d287c369e1cdda3b5e5e8c80bbc
Evan Bowman <bowmanevan@comcast.net>
2024-12-07 19:59:04 -0600

    Pivioted from orginal idea because we ran out of time. Russian roullete but with cards

--- Major Mash/Assets/Scripts/Card_Button.cs
+++ Major Mash/Assets/Scripts/Card_Button.cs
@@ -4,28 +4,28 @@ using UnityEngine;
 
 public class Card_Button : MonoBehaviour
 {
-    public int clickableValue = 25; //how much damage a card does
+    public int clickableValue = 25; // How much damage a card does
     
     private void OnMouseDown()
     {
         Turn_script manager = FindObjectOfType<Turn_script>();
-        MoveObject();
+        PlayCard(manager);
         ReducePlayerValue(manager.turn);
-        
         manager.whoseTurn();
         
-        Debug.Log("next turn");
+        Debug.Log("Next turn");
     }
 
-    private void MoveObject()
+    private void PlayCard(Turn_script manager)
     {
-        // Move the card to show it has been played
-        transform.position += new Vector3(-1f, 0f, 0f);
+        // This method is called when the card is played. 
+        // It handles the removal of the card.
+        Destroy(gameObject);  // Delete the card from the scene
     }
 
-    public void EnterObject() //when card is removed from the queue and neters the players hand
+    public void EnterObject() // When the card enters the player's hand (can be called when shuffled into hand)
     {
-       transform.position += new Vector3(0f, -1f, 0f); 
+        transform.position += new Vector3(0f, -1f, 0f); 
     }
 
     private void ReducePlayerValue(bool cardturn)
@@ -34,22 +34,18 @@ public class Card_Button : MonoBehaviour
         Player1_script player1 = FindObjectOfType<Player1_script>();
         Player2_script player2 = FindObjectOfType<Player2_script>();
 
-
-        if (player1 != null && player2 != null) //not sure what the point of this if statment is
+        if (player1 != null && player2 != null)
         {
-            if(cardturn)
+            if (cardturn)
             {
-            Debug.Log("player 2 health reduced");
-            player2.ReduceValue(clickableValue);
+                Debug.Log("Player 1 picked the evil card");
+                player1.ReduceValue(clickableValue);
             }
-
             else 
             {
-            Debug.Log("player 1 health reduced");
-            player1.ReduceValue(clickableValue);
+                Debug.Log("Player 2 picked the evil card");
+                player2.ReduceValue(clickableValue);
             }
-            
         }
     }
-
 }
--- Major Mash/Assets/Scripts/Deck_script.cs
+++ Major Mash/Assets/Scripts/Deck_script.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class ShuffleQueueScript : MonoBehaviour
 {
-    // An array or list to hold the 6 GameObjects
+    // An array or list to hold the 5 GameObjects
     public GameObject[] Cards;  // This will be assigned in the Inspector or dynamically
 
     // The Queue that will store the shuffled GameObjects
-    private Queue<GameObject> shuffledCards = new Queue<GameObject>(); //no memory leak, unity has a thing called garbage collecter which returns memory when program ends 
+    private Queue<GameObject> shuffledCards = new Queue<GameObject>();
+
+    // Define the positions where the cards should be placed (updated with 5 positions)
+    private Vector3[] positions = new Vector3[]
+    {
+        new Vector3(1.2f, 1.352f, 0f),
+        new Vector3(0.6f, 1.352f, 0f),
+        new Vector3(0f, 1.352f, 0f),
+        new Vector3(-0.6f, 1.352f, 0f),
+        new Vector3(-1.2f, 1.352f, 0f)
+    };
 
     void Start()
     {
@@ -24,22 +34,51 @@ public class ShuffleQueueScript : MonoBehaviour
         // Debugging: Print out the contents of the queue to check
         PrintQueue();
 
-        GameObject firstCard = shuffledCards.Dequeue();
-        Debug.Log("First card removed from the queue: " + firstCard.name);
-        EnterCard(firstCard);
+        // Step 3: Place each card at a random position
+        PlaceCardsAtRandomPositions();
+    }
+
+    private void PlaceCardsAtRandomPositions()
+    {
+        // Step 4: Ensure that there are exactly 5 positions and 5 cards
+        if (Cards.Length == 5 && positions.Length == 5)
+        {
+            // Shuffle the positions
+            ShufflePositions();
+
+            // Place each card at the shuffled position
+            for (int i = 0; i < Cards.Length; i++)
+            {
+                GameObject card = shuffledCards.Dequeue();
+                card.transform.position = positions[i];  // Set the position of each card
+                Debug.Log(card.name + " placed at " + positions[i]);
+            }
+        }
+        else
+        {
+            Debug.LogError("There should be exactly 5 cards and 5 positions!");
+        }
+    }
+
+    private void ShufflePositions()
+    {
+        // Shuffle the positions using the Fisher-Yates algorithm
+        for (int i = positions.Length - 1; i > 0; i--)
+        {
+            int j = Random.Range(0, i + 1);
+            Vector3 temp = positions[i];
+            positions[i] = positions[j];
+            positions[j] = temp;
+        }
     }
 
     private void EnterCard(GameObject card)
     {
-        
-        Card_Button cardController = card.GetComponent<Card_Button>(); //creates a variable to call the card button enterObject function
-
+        Card_Button cardController = card.GetComponent<Card_Button>(); // Call the card button's EnterObject function
         cardController.EnterObject();
-        
-        
     }
 
-    // Step 3: Shuffle function (using Fisher-Yates algorithm)
+    // Step 5: Shuffle function (using Fisher-Yates algorithm)
     void Shuffle(GameObject[] array)
     {
         for (int i = array.Length - 1; i > 0; i--)
@@ -55,12 +94,10 @@ public class ShuffleQueueScript : MonoBehaviour
     void PrintQueue()
     {
         Debug.Log("Shuffled Queue:");
-
         foreach (var obj in shuffledCards)
         {
             Debug.Log(obj.name);
         }
     }
 }
-
 
